b8be41359beba871c7d7bd7f59c2aefa8a552262
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-06-19 22:29:22 +0100

    added some fonts and colors

--- app/src/main/java/com/example/fournitutreappuicompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/fournitutreappuicompose/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFFCEBB9E)
+val SmallIndication =Color(0xFF645F57)
+val ButtonColor=Color(0xFF62442B)
+val EntryText = Color(0xFF262525)

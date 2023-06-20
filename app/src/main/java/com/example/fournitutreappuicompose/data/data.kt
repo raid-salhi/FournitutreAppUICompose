1fe1a57948beef41d7a481ca4f040fa26406a2c0
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-06-20 14:48:47 +0100

    Finished the hompage

--- app/src/main/java/com/example/fournitutreappuicompose/data/data.kt
+++ app/src/main/java/com/example/fournitutreappuicompose/data/data.kt
@@ -1,13 +1,14 @@
 package com.example.fournitutreappuicompose.data
 
+import com.example.fournitutreappuicompose.R
 import com.example.fournitutreappuicompose.model.Fourniture
 
 class Data {
     fun getAllFourniture() : List<Fourniture>{
         return listOf(
-            Fourniture("Leatherette Sofa",12,15.18,"Yellow"),
-            Fourniture("Ork Stool",12,16.20,"Black Limba"),
-            Fourniture("Royal Palm Lamp",12,19.99,"Dark Navy")
+            Fourniture("Leatherette Sofa", R.drawable.sofa,15.18,"Yellow"),
+            Fourniture("Ork Stool",R.drawable.stoll,16.20,"Black Limba"),
+            Fourniture("Royal Palm Lamp",R.drawable.lampa,19.99,"Dark Navy")
         )
     }
 }

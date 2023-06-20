54f50def5d4c97aca6e4ac07b145cd6a7311425d
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-06-20 09:35:22 +0100

    added screen and nav

--- app/src/main/java/com/example/fournitutreappuicompose/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/fournitutreappuicompose/navigation/AppNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.fournitutreappuicompose.screens.HomeScreen
 import com.example.fournitutreappuicompose.screens.MarketingScreen
 
 @Composable
@@ -12,6 +13,9 @@ fun AppNavigation (){
     NavHost(navController = navController, startDestination = ScreensNavigation.MarketingScreen.name ){
         composable(route = ScreensNavigation.MarketingScreen.name){
             MarketingScreen(navController)
+        }
+        composable(route = ScreensNavigation.HomeScreen.name){
+            HomeScreen(navController = navController)
         }
 
     }
---
+++ app/src/main/java/com/example/fournitutreappuicompose/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.fournitutreappuicompose.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController){
+
+}
